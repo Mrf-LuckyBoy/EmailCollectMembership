02fd404a45aa3f0328027d54012b33d8adca2e84
Atsawa Chakhonmettakul <atsawa.ch@inet.co.th>
2025-07-02 15:30:30 +0700

    add telegram noti

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import corn from 'node-cron';
 import { errorMonitor } from 'node:events';
+import axios from 'axios';
+import dotenv from 'dotenv';
 import sendMail from './mail.js';
 
+dotenv.config();
 corn.schedule('0 10 3 * *', async () => {
   try {
     const info = await sendMail();
@@ -11,8 +14,16 @@ corn.schedule('0 10 3 * *', async () => {
         console.log('Mail sent:', mailResult.response);
       })
     );
+    await axios.post(`${process.env.domain_tele}/bot${process.env.chat_bot_tele}/sendMessage`, {
+      chat_id: process.env.chat_id_tele,
+      text: 'Send email successfuly',
+    });
   } catch (e) {
     console.error('Error sending mail:', errorMonitor.message);
+    await axios.post(`${process.env.domain_tele}/bot${process.env.chat_bot_tele}/sendMessage`, {
+      chat_id: process.env.chat_id_tele,
+      text: 'Send email Unsuccessfuly',
+    });
   }
 });
 
